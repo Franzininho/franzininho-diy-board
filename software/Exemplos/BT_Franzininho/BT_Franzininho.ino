eff4d87d1a80bf3be3849d095e36012cfce72336
Fábio Souza <fabio_souza53@hotmail.com>
2017-06-17 08:30:27 -0300

    Exemplos
    Colocado mais exemplos

---
+++ software/Exemplos/BT_Franzininho/BT_Franzininho.ino
@@ -0,0 +1,30 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2, 3); // RX, TX
+
+void setup() {
+
+  pinMode(1, OUTPUT);
+   // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+   if (mySerial.available()){
+
+    
+    int cmd = mySerial.read();
+    if(cmd == 'f'){
+      digitalWrite(1, HIGH);
+    }
+    else if(cmd == 'g'){
+      digitalWrite(1, LOW);
+    }
+   }
+   delay(500);
+  
+}
+
