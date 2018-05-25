0e75ea3359d26773d6d13bcd014690470442f64e
Fabio Souza <fs.embarcados@gmail.com>
2018-05-25 14:39:47 -0300

    Organização das pasta e versões

--- software/AtmelStudio/ArduinoSketchFranizinho/ArduinoSketchFranizinho/ArduinoCore/Debug/src/core/new.d
+++
@@ -1,7 +0,0 @@
-src/core/new.d src/core/new.o: ../src/core/new.cpp \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
